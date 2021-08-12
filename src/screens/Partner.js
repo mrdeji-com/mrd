1b5c8086203ccc24ded37038d8ed9d2a8978b616
Eniola <69263854+Ayodeji10@users.noreply.github.com>
2021-08-12 21:06:42 +0100

    form-revert

--- src/screens/Partner.js
+++ src/screens/Partner.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 // import { link } from 'react-router-dom';
 import Navbar from "../components/nav";
 import Carousel from "../components/carousel";
 import Footer from "../components/footer";
 
-import { InlineWidget } from "react-calendly";
+// import { InlineWidget } from "react-calendly";
 
 const Partner = () => {
+  const [option1, setOption1] = useState(false)
+  const [option2, setOption2] = useState(false)
+  const [option3, setOption3] = useState(false)
+  const [option4, setOption4] = useState(false)
+
   return (
     <div class="container-fluid">
       <Navbar />
@@ -35,7 +40,114 @@ const Partner = () => {
                 </div>
               </div>
               <div className="col-lg-6 col-md-6 form-body">
-                <InlineWidget url="https://calendly.com/mrdeji/discuss-potential-partnerships" />
+                {/* <InlineWidget url="https://calendly.com/mrdeji/discuss-potential-partnerships" /> */}
+                <div className="input">
+                  <label htmlFor="name"><img src={require("../img/Vector1.svg").default} alt="img" />Full Name <span className="required">*</span></label>
+                  <input type="text" id="name" />
+                </div>
+                <div className="input">
+                  <label htmlFor="email"><img src={require("../img/Group 4.svg").default} alt="img" />Contact Email <span className="required">*</span></label>
+                  <input type="text" id="email" />
+                </div>
+                <div className="input">
+                  <label htmlFor="number"><img src={require("../img/Vector2.svg").default} alt="img" />Phone Number <span className="required">*</span></label>
+                  <input type="text" id="number" />
+                </div>
+                <div className="input">
+                  <label htmlFor="Interest"><img src={require("../img/Group 6.svg").default} alt="img" />Partnership Interest</label>
+                  <div className="interests">
+                    <h5>Choose area(s) you would love to partner with us</h5>
+                    <h6><img src={require("../img/information.svg").default} alt="img" />Hover on each interest to see description</h6>
+                    <div className="interest">
+                      <div className="row">
+                        <div className="col-xl-5 col-lg-12 col-md-12" onMouseEnter={() => setOption1(true)} onMouseLeave={() => setOption1(false)}>
+                          <input type="radio" id="tech-support" /><label htmlFor="tech-support">Tech Service
+                            Provider</label>
+                        </div>
+                        <div className="col-xl-7 col-lg-12 col-md-12">
+                          {
+                            option1 &&
+                            <span className="interest-modal"><img src={require("../img/information.svg").default} alt="img" />
+                              <span>
+                                Do you offer services around web development, payment integration or
+                                related tech services and you would love to partner with us?
+                              </span>
+                            </span>
+                          }
+                        </div>
+                      </div>
+                    </div>
+                    <div className="interest">
+                      <div className="row">
+                        <div className="col-xl-5 col-lg-12 col-md-12" onMouseEnter={() => setOption2(true)} onMouseLeave={() => setOption2(false)}>
+                          <input type="radio" id="value-added" /><label htmlFor="value-added">Value Added
+                            Distributor</label>
+                        </div>
+                        <div className="col-xl-7 col-lg-12 col-md-12">
+                          {
+                            option2 &&
+                            <span className="interest-modal"><img src={require("../img/information.svg").default} alt="img" />
+                              <span>
+                                This is for Value Added distributor who is interested in our
+                                solutions
+                              </span>
+                            </span>
+                          }
+                        </div>
+                      </div>
+                    </div>
+                    <div className="interest">
+                      <div className="row">
+                        <div className="col-xl-5 col-lg-4 col-md-4 col-sm-3" onMouseEnter={() => setOption3(true)} onMouseLeave={() => setOption3(false)}>
+                          <input type="radio" id="reseller" /><label htmlFor="reseller">Reseller</label>
+                        </div>
+                        <div className="col-xl-7 col-lg-8 col-md-8 col-sm-9">
+                          {
+                            option3 &&
+                            <span className="interest-modal"><img src={require("../img/information.svg").default} alt="img" />
+                              <span>
+                                Do you offer services around web development, payment integration or
+                                related tech services and you would love to partner
+                                with us?
+                              </span>
+                            </span>
+                          }
+                        </div>
+                      </div>
+                    </div>
+                    <div className="interest">
+                      <div className="row">
+                        <div className="col-xl-5 col-lg-12 col-md-12" onMouseEnter={() => setOption4(true)} onMouseLeave={() => setOption4(false)}>
+                          <input type="radio" id="service-consultant" /><label htmlFor="service-consultant">Service Consultant</label>
+                        </div>
+                        <div className="col-xl-7 col-lg-12 col-md-12">
+                          {
+                            option4 &&
+                            <span className="interest-modal"><img src={require("../img/information.svg").default} alt="img" />
+                              <span>
+                                This is for individual or businesses that deployes digital
+                                solutions
+                              </span>
+                            </span>
+                          }
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="input">
+                  <label htmlFor="letter"><img src={require("../img/menu.svg").default} alt="img" />Additional Information</label>
+                  <textarea name id="letter" placeholder="Do you have any other relevant information for us? (optional)" defaultValue={""} />
+                </div>
+                <div className="row">
+                  <div className="col-1"><input type="checkbox" id="checkbox" /></div>
+                  <div className="col-11">
+                    <p>I've read <a href>Privacy Policy</a> the below and consent the processing of my
+                      data as part of my job
+                      application.
+                    </p></div>
+                </div>
+                <button>SUBMIT <img src={require("../img/Vector3.svg").default} alt="img" /></button>
               </div>
             </div>
           </div>
